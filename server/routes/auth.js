b93338dbb5591d94b93928aeb9d44bdb4f28b3ae
htg-18 <hs553608@gmail.com>
2023-12-19 16:31:22 +0530

    Saving data to mongoDB

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,37 @@
-const express=require('express')
-const User =require('../models/Users')
-const router=express.Router()
+const express = require('express');
+const User = require('../models/Users');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post("/",(req,res)=>{
-    const user=User(req.body)
-    user.save()
-    res.send("auth")
-})
+router.post('/createuser', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports=router
+    try {
+        const existingUser = await User.findOne({ email: req.body.email });
+
+        if (existingUser) {
+            return res.status(400).json({ error: "User with this email already exists" });
+        }
+
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        await user.save();
+        res.status(201).json(user); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+module.exports = router;
